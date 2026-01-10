6b275be8bdd6beb7249716edd9ff738a9243d14c
Riteshkumaran <riteshkumaran2005@gmail.com>
2026-01-10 11:34:26 +0530

    Add files via upload

---
+++ Project/Frontend/JS/equality_operator.js
@@ -0,0 +1,5 @@
+var a = 20;
+var b = "20";
+console.log(a==20);
+console.log(a==b);
+console.log(a===b);
---
+++ Project/Frontend/JS/operator.js
@@ -0,0 +1,8 @@
+console.log("Calculator");
+var a = 11;
+var b = 5;
+console.log("Addition : "+(a+b));
+console.log("Subtraction : "+(a-b));
+console.log("Multiplication : "+(a*b));
+console.log("Division : "+(a/b));
+console.log("Modulus : "+(a%b));
